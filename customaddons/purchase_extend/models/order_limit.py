ff9287ec3a02a2604da61ec59abcabbc700a5a1f
Son <son79628@st.vimaru.edu.vn>
2023-02-07 13:26:03 +0700

    Update delete

--- customaddons/purchase_extend/models/order_limit.py
+++
@@ -1,6 +0,0 @@
-from odoo import models, fields
-
-
-class OrderLimit(models.Model):
-    _name = 'order.limit'
-    employee_order_limit = fields.Many2many('employee.order.limit', string="Employee order limit")
